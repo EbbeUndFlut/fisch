98e0e69fb8bca322e64c5f92095d9b410599f8ec
Christian <c.peters@makermaker.de>
2022-09-08 14:08:45 +0200

    Added Herosection with module css

---
+++ src/components/herosection/HeroSection.jsx
@@ -0,0 +1,12 @@
+import style from './HeroSection.module.css'
+
+const HeroSection = () => {
+    return (
+        <section className={style.hero}>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit laboriosam obcaecati eveniet magni dolorem, necessitatibus explicabo at facilis quae laborum.</p>
+
+        </section>
+    )
+}
+
+export default HeroSection
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
+import HeroSection from "../components/herosection/HeroSection"
+
 const Home =  () => {
-    return <h1>Home</h1>
+    return <HeroSection />
 }
 
 export default Home
