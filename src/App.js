75c6f600e51499419cc11223e3fe49d8e21d004d
Christian <c.peters@makermaker.de>
2022-09-08 14:14:45 +0200

    Added the money shop

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Shop from './pages/Shop';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/shop' element={<Shop />} />
         </Routes>
       </BrowserRouter>
 
