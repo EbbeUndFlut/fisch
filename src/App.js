9ae4e84bf769633f49459811705297ec73f3aeaa
Christian <c.peters@makermaker.de>
2022-09-08 14:24:42 +0200

    member

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Member from './pages/Member';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/member' element={<Member />} />
         </Routes>
       </BrowserRouter>
 
